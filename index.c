f669d070be651d2625e22fee6ea5beab4353cdc1
JeovaneLek <149844425+JeovaneLek@users.noreply.github.com>
2026-02-12 21:56:21 +0000

    .

--- index.c
+++ index.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(void) {
+    char nome[50];
+    int idade;
+    char estdcvl[10];
+
+    printf("Qual seu nome?: ");
+    scanf(%f, &nome);
+    printf("Qual sua idade?: ");
+    scanf(%d, &idade);
+    printf("Qual seu estado civil?: ");
+    scanf(%f, &estdcvl);
+    system("cls");
+    printf("Ficha de " + nome":");
+    printf("Nome: " + nome);
+    printf("Idade: " + idade);
+    printf("Estado Civil: " + estdcvl);
+
+}
